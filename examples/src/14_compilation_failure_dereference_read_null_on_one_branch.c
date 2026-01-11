937ea45b7711333de84ac49c0bbe1682e46b3af5
Josh Neil <joshneil8@googlemail.com>
2026-01-11 20:31:21 +0000

    Add example where it catches the fact that a pointer may or may not be NULL

---
+++ examples/src/14_compilation_failure_dereference_read_null_on_one_branch.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NC_PTR_SAFE_FILE_NAME fixed_compilation_dereference_read_null_on_one_branch_c
+
+#include "null_checked_ptr.h"
+
+int main() {
+    int foo = 2;
+    NC_PTR(int*, my_nc_ptr) = &foo;
+    int my_nc_ptr_value = 0;
+    
+    if(rand() & 1) {
+        NC_PTR_ASSIGN(my_nc_ptr, NULL);
+    }
+
+    /*Errors because the pointer MAY be NULL */
+    NC_PTR_DEREFERENCE_READ(my_nc_ptr_value, my_nc_ptr);
+    assert(my_nc_ptr_value == 2);
+    printf("my_nc_ptr_value=%i line=%i\n", my_nc_ptr_value, __LINE__);
+
+    return 0;
+}
---
+++ examples/src/15_fixed_compilation_dereference_read_null_on_one_branch.c
@@ -0,0 +1,32 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NC_PTR_SAFE_FILE_NAME compilation_failure_dereference_read_null_c
+
+#include "null_checked_ptr.h"
+
+int main() {
+    int foo = 2;
+    NC_PTR(int*, my_nc_ptr) = &foo;
+    int my_nc_ptr_value = 0;
+    
+    if(rand() & 1) {
+        NC_PTR_ASSIGN(my_nc_ptr, NULL);
+    }
+
+    /*
+        If we do a null check, then the compiler will know that the pointer is
+        not NULL
+    */
+    if (!NC_PTR_IS_NULL(my_nc_ptr)) {
+        NC_PTR_DEREFERENCE_READ(my_nc_ptr_value, my_nc_ptr);
+        assert(my_nc_ptr_value == 2);
+        printf("my_nc_ptr_value=%i line=%i\n", my_nc_ptr_value, __LINE__);
+    }
+    else {
+        printf("my_nc_ptr_value is NULL line=%i\n", __LINE__);
+    }
+
+    return 0;
+}
